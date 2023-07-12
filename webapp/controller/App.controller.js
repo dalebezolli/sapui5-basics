4b07635bd9d2a95b6ab3d63ab7f572123f71e02c
Dale Bezolli <pandelibezolli@gmail.com>
2023-07-12 12:00:25 +0000

    Create a counter app

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,18 @@
-sap.ui.define([ 'sap/ui/core/mvc/Controller' ], Controller => {
+sap.ui.define([ 
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel',
+], (Controller, JSONModel) => {
     'use strict';
 
     return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
-        onDoSomething: _ => {
-            alert('Something fun');
+        onInit: function() {
+            const model = new JSONModel({ count: 0 });
+            this.getView().setModel(model);
+        },
+        onCount: function(event) {
+            const model = this.getView().getModel();
+            const prevCount = model.getProperty('/count');
+            model.setProperty('/count', prevCount + 1);
         }
     });
 });
